fed98c8dff0752201eabde262823ed081f886360
mcordovilla <mcordovilla@vocces.com>
2021-06-18 19:35:50 +0200

    Barra de búsqueda en foros y grupos creada

--- app/src/main/java/com/example/yosigo/Facilitador/ForumsFacilitador/ForumsListFragment.java
+++ app/src/main/java/com/example/yosigo/Facilitador/ForumsFacilitador/ForumsListFragment.java
@@ -1,11 +1,14 @@
 package com.example.yosigo.Facilitador.ForumsFacilitador;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -26,7 +29,10 @@ import java.util.Map;
 public class ForumsListFragment extends Fragment {
 
     private ForumsListViewModel ViewModelForums;
+    private View root;
     private ListView list;
+    private EditText searchBar;
+
     private List<String> nameList = new ArrayList<>();
     private Map<String, String> forumsMap = new HashMap<>();
 
@@ -34,9 +40,10 @@ public class ForumsListFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         ViewModelForums =
                 new ViewModelProvider(this).get(ForumsListViewModel.class);
-        View root = inflater.inflate(R.layout.list_forum_facilitador, container, false);
+        root = inflater.inflate(R.layout.list_forum_facilitador, container, false);
 
         list = root.findViewById(R.id.list_forums);
+        searchBar = root.findViewById(R.id.search_forum_name);
 
         ViewModelForums.getText().observe(getViewLifecycleOwner(), new Observer<Map<String, String>>() {
             @Override
@@ -61,6 +68,20 @@ public class ForumsListFragment extends Fragment {
             }
         });
 
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String newText = editable.toString();
+                filterForums(newText);
+            }
+        });
+
         FloatingActionButton fab = root.findViewById(R.id.fab_forum);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,5 +91,46 @@ public class ForumsListFragment extends Fragment {
         });
 
         return root;
+    }
+
+    private void filterForums(String text) {
+        if(text.isEmpty()){
+            nameList.clear();
+            for (String key : forumsMap.keySet()) {
+                nameList.add( (String) key);
+            }
+
+            ArrayAdapter<String> adapter = new ActivityListAdapter(root.getContext(), forumsMap, nameList);
+            list.setAdapter(adapter);
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Bundle bundle = new Bundle();
+                    bundle.putString("param1", "Forum");
+                    bundle.putString("param2", forumsMap.get(nameList.get(position)));
+                    Navigation.findNavController(view).navigate(R.id.action_nav_forums_to_chatFacilitadorFragment, bundle);
+                }
+            });
+        } else {
+            Map<String, String> filterMap = new HashMap<>();
+            nameList.clear();
+            for (String key : forumsMap.keySet()) {
+                if( key.toLowerCase().contains(text.toLowerCase()) ) {
+                    nameList.add((String) key);
+                    filterMap.put(key, forumsMap.get(key));
+                }
+            }
+            ArrayAdapter<String> adapter = new ActivityListAdapter(root.getContext(), filterMap, nameList);
+            list.setAdapter(adapter);
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Bundle bundle = new Bundle();
+                    bundle.putString("param1", "Forum");
+                    bundle.putString("param2", filterMap.get(nameList.get(position)));
+                    Navigation.findNavController(view).navigate(R.id.action_nav_forums_to_chatFacilitadorFragment, bundle);
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/yosigo/Facilitador/Groups/GroupListFragment.java
+++ app/src/main/java/com/example/yosigo/Facilitador/Groups/GroupListFragment.java
@@ -10,11 +10,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.example.yosigo.R;
@@ -31,6 +34,8 @@ public class GroupListFragment extends Fragment {
     private GroupListViewModel mViewModel;
     private View root;
     private ListView list;
+    private EditText searchBar;
+
     private Map<String, String> groupsMap = new HashMap<>();
     private List<String> groupsNameList = new ArrayList<>();
 
@@ -45,6 +50,7 @@ public class GroupListFragment extends Fragment {
         root =  inflater.inflate(R.layout.group_list_fragment, container, false);
 
         list = root.findViewById(R.id.list_groups);
+        searchBar = root.findViewById(R.id.search_group_name);
 
         mViewModel.getNames().observe(getViewLifecycleOwner(), new Observer<List<String>>() {
             @Override
@@ -62,6 +68,20 @@ public class GroupListFragment extends Fragment {
             }
         });
 
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String newText = editable.toString();
+                filterGroups(newText);
+            }
+        });
+
         FloatingActionButton fab = root.findViewById(R.id.fab_groups);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,4 +93,28 @@ public class GroupListFragment extends Fragment {
         return root;
     }
 
+    private void filterGroups(String text) {
+        if(text.isEmpty()){
+            ArrayAdapter<String> adapter = new ArrayAdapter(
+                    root.getContext(),
+                    android.R.layout.simple_list_item_1,
+                    groupsNameList
+            );
+            list.setAdapter(adapter);
+        } else {
+            List<String> filterList = new ArrayList<>();
+            filterList.clear();
+            for (String name : groupsNameList) {
+                if( name.toLowerCase().contains(text.toLowerCase()) ) {
+                    filterList.add(name);
+                }
+            }
+            ArrayAdapter<String> adapter = new ArrayAdapter(
+                    root.getContext(),
+                    android.R.layout.simple_list_item_1,
+                    filterList
+            );
+            list.setAdapter(adapter);
+        }
+    }
 }
